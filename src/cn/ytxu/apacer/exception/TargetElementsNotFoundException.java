6d4091b9b93db48248e014ce37135cf97e667e38
ytxu <roy@newchama.com>
2016-05-03 16:57:43 +0800

    添加两个自定义异常；

---
+++ src/cn/ytxu/apacer/exception/TargetElementsNotFoundException.java
@@ -0,0 +1,12 @@
+package cn.ytxu.apacer.exception;
+
+/**
+ * Created by ytxu on 16/5/3.
+ * 目标元素没有找到
+ */
+public class TargetElementsNotFoundException extends Exception {
+
+    public TargetElementsNotFoundException(String msg) {
+        super(msg);
+    }
+}
