72800cbc1eef3585457215aa4fcb8bf0b74d2a86
xuyt11 <xuytao11@163.com>
2016-05-08 16:16:12 +0800

    refactor request and statusCode config;

---
+++ src/cn/ytxu/apacer/system_platform/RequestConfig.java
@@ -0,0 +1,48 @@
+package cn.ytxu.apacer.system_platform;
+
+public class RequestConfig {
+
+    private static RequestConfig instance;
+    private String requestFileDir;// 自动生成的请求文件所在目录
+
+    private RequestConfig(String requestFileDir) {
+        this.requestFileDir = requestFileDir;
+    }
+
+    public static RequestConfig getInstance(String requestFileDir) {
+        if (instance == null) {
+            instance = new RequestConfig(requestFileDir);
+        }
+        return instance;
+    }
+
+    /**
+     * 接口文件保存目录
+     */
+    private final String DirPath = requestFileDir + "%s/api/";
+    /**
+     * 接口文件的包名
+     */
+    private final String PackageName = "com.newchama.api.%s.api";
+    /**
+     * API中所有url的头部
+     */
+    public final String StartUrl = "http://test.newchama.com";
+    public final String ApiDocUrl = "http://apidoc.newchama.com";// API文档的url
+    public final String BaseApiFileName = "BaseApi";// 所有http接口中的基类
+    private final String PublicApiFileName = "HttpApi%s";// 公开给外部调用的Http类
+    // 将参数名称从context改为cxt001，防止在之后出现参数名称为context的参数。
+    public final String AndroidContextParamName = "cxt001";
+
+    public String getPackageName(String versionCode) {
+        return String.format(PackageName, versionCode);
+    }
+
+    public String getDirPath(String versionCode) {
+        return String.format(DirPath, versionCode);
+    }
+
+    public String getPublicApiFileName(String formatVersionCode) {
+        return String.format(PublicApiFileName, formatVersionCode);
+    }
+}
