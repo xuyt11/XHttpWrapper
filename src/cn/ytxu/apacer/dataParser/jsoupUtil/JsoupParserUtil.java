9b1987cd5036561ff7056a0fa9754e6a22e1a5e5
xuyt11 <xuytao11@163.com>
2016-05-14 16:31:48 +0800

    将jsoup中的解析api提取到公共的工具类中--JsoupParserUtil;

---
+++ src/cn/ytxu/apacer/dataParser/jsoupUtil/JsoupParserUtil.java
@@ -0,0 +1,51 @@
+package cn.ytxu.apacer.dataParser.jsoupUtil;
+
+import cn.ytxu.apacer.config.Config;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.select.Selector;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by ytxu on 2016/5/14.
+ */
+public class JsoupParserUtil {
+
+    public static Document getDocument(String apiDocHtmlPath) {
+//		Connection conn = Jsoup.connect(ApiEnitity.ApiDocUrl);
+//		conn.userAgent(UserAgentConfig.getWithRandom());
+        try {
+            Document doc = Jsoup.parse(new File(apiDocHtmlPath), Config.CharsetName);
+            return doc;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("ytxu document is null, so can not run...");
+        }
+    }
+
+    public static Elements getEles(Document doc, String cssQuery) {
+        return Selector.select(cssQuery, doc);
+    }
+
+    public static Elements getEles(Elements eles, String cssQuery) {
+        return Selector.select(cssQuery, eles);
+    }
+
+    public static Elements getEles(Element ele, String cssQuery) {
+        return Selector.select(cssQuery, ele);
+    }
+
+    public static String getText(Element ele) {
+        return ele.text().trim();
+    }
+
+
+
+
+
+
+}
