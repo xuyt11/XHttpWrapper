82c6d048dc9e6565fae93bfb295e3bc5b4f55a7d
xuyt11 <xuytao11@163.com>
2016-07-10 22:29:34 +0800

    转换配置文件中的${}数据；

--- src/cn/ytxu/api_semi_auto_creater/creater/Property.java
+++ src/cn/ytxu/api_semi_auto_creater/creater/Property.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by ytxu on 2016/7/10.
@@ -35,6 +37,10 @@ public class Property {
 
     public static String getValue(String key) {
         Property property = data.get(key);
+        if (property == null) {// not found target property
+            throw new RuntimeException("can not found target property, key:" + key);
+        }
+
         if (property.val != null) {
             return property.val;
         }
@@ -159,10 +165,12 @@ public class Property {
             }
 
             if (property.val != null) {
-
+                property.val = new RegConvert(property.val).start();
             } else {// property.values must be have
                 List<Value> values = property.values;
-
+                for (Value value : values) {
+                    value.val = new RegConvert(value.val).start();
+                }
             }
         }
 
@@ -177,4 +185,58 @@ public class Property {
         }
 
     }
+
+    static class RegConvert {
+        private static final String regex = "\\$\\{\\w+\\}";
+
+        private Pattern p = Pattern.compile(regex);
+        private String str;
+        private List<String> groups;
+
+        public RegConvert(String str) {
+            this.str = str;
+            groups = new ArrayList<>();
+        }
+
+        public String start() {
+            findAndAddMatchedGroups();
+            loopConvert();
+            return str;
+        }
+
+        private void findAndAddMatchedGroups() {
+            Matcher m = p.matcher(str);
+            while (m.find()) {
+                addGroup(m);
+            }
+        }
+
+        private void addGroup(Matcher m) {
+            int start = m.start();
+            int end = m.end();
+            String group = m.group();
+            System.out.println("group start:" + start + ", group end:" + end + ", group:" + group);
+            groups.add(group);
+        }
+
+        private void loopConvert() {
+            for (String group : groups) {
+                convert(group);
+            }
+        }
+
+        private void convert(String group) {
+            String key = group.substring(2, group.length() - 1);
+            String value;
+            try {
+                value = getValue(key);
+            } catch (Exception ignore) {
+                return;
+            }
+
+            str = str.replace(group, value);
+        }
+
+    }
+
 }
