cd9d29ec1956fd2556a94891775197d328305902
xuyt11 <xuytao11@163.com>
2016-07-16 22:51:14 +0800

    extract object method for xml parse util

--- src/cn/ytxu/api_semi_auto_creater/creater/Property.java
+++ src/cn/ytxu/api_semi_auto_creater/creater/Property.java
@@ -1,12 +1,9 @@
 package cn.ytxu.api_semi_auto_creater.creater;
 
 import cn.ytxu.api_semi_auto_creater.config.OSPlatform;
+import cn.ytxu.api_semi_auto_creater.util.XmlParseUtil;
 import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -54,70 +51,34 @@ public class Property {
         throw new IllegalStateException("can not found valite data in this os");
     }
 
-    static class PropertyParser {
-        private static final String PropertyXmlName = "NewChama-android-property.xml";
-
+    static class PropertyParser extends XmlParseUtil implements XmlParseUtil.Callback {
         private Property property;
         private Property.Value value;
 
         public void init() {
-            try {
-                InputStream xml = getXml();
-                XmlPullParser pullParser = getPullParser(xml);
-                loopParse(pullParser);
-                close(xml);
-            } catch (XmlPullParserException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
+            parseProperty(this);
+        }
+
+        @Override
+        public void startDocument(XmlPullParser pullParser, String nodeName) {
+            initData();
+        }
+
+        @Override
+        public void startTag(XmlPullParser pullParser, String nodeName) {
+            if ("property".equals(nodeName)) {
+                initProperty(pullParser);
+            } else if ("value".equals(nodeName)) {
+                setValues(pullParser);
             }
         }
 
-        private InputStream getXml() {
-            InputStream xml = Property.class.getClassLoader().getResourceAsStream(PropertyXmlName);
-            return xml;
-        }
-
-        private XmlPullParser getPullParser(InputStream xml) throws XmlPullParserException {
-            // 获得pull解析器工厂
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            // 获取XmlPullParser的实例
-            XmlPullParser pullParser = factory.newPullParser();
-            // 设置需要解析的XML数据
-            pullParser.setInput(xml, "UTF-8");
-            return pullParser;
-        }
-
-        private void loopParse(XmlPullParser pullParser) throws XmlPullParserException, IOException {
-            int event = pullParser.getEventType();// 取得事件
-            while (event != XmlPullParser.END_DOCUMENT) {// 若为解析到末尾，文档结束
-                String nodeName = pullParser.getName();// 节点名称
-                parse(pullParser, event, nodeName);
-                event = pullParser.next(); // 下一个标签
-            }
-        }
-
-        private void parse(XmlPullParser pullParser, int event, final String nodeName) throws XmlPullParserException, IOException {
-            switch (event) {
-                case XmlPullParser.START_DOCUMENT: // 文档开始
-                    initData();
-                    break;
-                case XmlPullParser.START_TAG: // 标签开始
-                    if ("property".equals(nodeName)) {
-                        initProperty(pullParser);
-                    } else if ("value".equals(nodeName)) {
-                        setValues(pullParser);
-                    }
-                    break;
-                case XmlPullParser.END_TAG: // 标签结束
-                    if ("property".equals(nodeName)) {
-                        addProperty();
-                    } else if ("value".equals(nodeName)) {
-                        addValues();
-                    }
-                    break;
+        @Override
+        public void endTag(XmlPullParser pullParser, String nodeName) {
+            if ("property".equals(nodeName)) {
+                addProperty();
+            } else if ("value".equals(nodeName)) {
+                addValues();
             }
         }
 
@@ -132,16 +93,13 @@ public class Property {
             property.val = pullParser.getAttributeValue(null, "val");
         }
 
-        private void setValues(XmlPullParser pullParser) throws XmlPullParserException, IOException {
+        private void setValues(XmlPullParser pullParser) {
             if (property.values == null) {
                 property.values = new ArrayList<>();
             }
             value = new Value();
             value.key = pullParser.getAttributeValue(null, "key");
             value.val = pullParser.getAttributeValue(null, "val");
-//        value.key = pullParser.getAttributeValue(0);
-//        value.val = pullParser.getAttributeValue(1);
-//        value.val = pullParser.nextText();
         }
 
         private void addValues() {
@@ -176,12 +134,6 @@ public class Property {
 
         private boolean notHasPreviousElement() {
             return data.size() <= 0;
-        }
-
-        private void close(InputStream xml) throws IOException {
-            if (xml != null) {
-                xml.close();
-            }
         }
 
     }
