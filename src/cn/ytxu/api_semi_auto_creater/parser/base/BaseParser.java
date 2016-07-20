ee43b1bca37abac49b8cb2e9bd5a5933c7a93761
xuyt11 <xuytao11@163.com>
2016-07-21 01:12:22 +0800

    解析了Section中的request的name与version code；

--- src/cn/ytxu/api_semi_auto_creater/parser/base/BaseParser.java
+++ src/cn/ytxu/api_semi_auto_creater/parser/base/BaseParser.java
@@ -2,6 +2,8 @@ package cn.ytxu.api_semi_auto_creater.parser.base;
 
 import cn.ytxu.apacer.config.Config;
 import cn.ytxu.apacer.dataParser.jsoupUtil.JsoupParserUtil;
+import cn.ytxu.util.CamelCaseUtils;
+import cn.ytxu.util.LogUtil;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -22,11 +24,12 @@ public class BaseParser {
     public void start() {
         DocEntity doc = new DocParser().start();
         for (DocEntity.SectionEntity section : doc.getSections()) {
-
-
+            new SectionParser(section).start();
         }
-    }
 
+        
+
+    }
 
     public static class DocParser {
         private static final String CONTENT = "div.container-fluid > div.row-fluid > div#content";
@@ -108,4 +111,70 @@ public class BaseParser {
 
     }
 
+    public static class SectionParser {
+        private static final String CSS_QUERY_ARTICLE = "article";
+        private static final String ATTR_DATA_NAME = "data-name";
+        private static final String ATTR_DATA_VERSION = "data-version";
+
+        private DocEntity.SectionEntity sectionEntity;
+
+        public SectionParser(DocEntity.SectionEntity section) {
+            super();
+            sectionEntity = section;
+        }
+
+        public void start() {
+            // 1、生成一个需要搜索到的div元素中，id属性的开始字符串
+            String cssQuery = getCssQuery();
+            // 2、搜索到属性值开头匹配的所有div元素
+            Elements requestEles = getRequestElements(cssQuery);
+
+            getRequests(requestEles);
+        }
+
+        private String getCssQuery() {
+            String replaceName = sectionEntity.getName().replace(" ", "_");// 防止出现类似：fa deal的分类，而在检索的id时，是以fa_deal开始的；
+            String startId = "api-" + replaceName + "-";
+            String cssQuery = "div[id^=" + startId + "]";// 匹配属性值开头：匹配以api-Account-开头的所有div元素
+            return cssQuery;
+        }
+
+        private Elements getRequestElements(String cssQuery) {
+            return JsoupParserUtil.getEles(sectionEntity.getElement(), cssQuery);
+        }
+
+        private void getRequests(Elements requestEles) {
+            if (null == requestEles || requestEles.size() <= 0) {
+                LogUtil.i("ytxu can`t find a method element of the category...");
+                return;
+            }
+
+            List<DocEntity.RequestEntity> requests = new ArrayList<>(requestEles.size());
+            for (Element requestEle : requestEles) {
+                DocEntity.RequestEntity request = getRequest(requestEle);
+                requests.add(request);
+            }
+            sectionEntity.setRequests(requests);
+        }
+
+        private DocEntity.RequestEntity getRequest(Element requestEle) {
+            DocEntity.RequestEntity request = new DocEntity.RequestEntity(sectionEntity, requestEle);
+
+            Element articleEle = JsoupParserUtil.getFirstEle(requestEle, CSS_QUERY_ARTICLE);
+            String dataName = JsoupParserUtil.getAttr(articleEle, ATTR_DATA_NAME);
+            String methodName = CamelCaseUtils.toCamelCase(dataName);
+            request.setName(methodName);
+
+            String methodVersion = JsoupParserUtil.getAttr(articleEle, ATTR_DATA_VERSION);
+            request.setVersion(methodVersion);
+
+            return request;
+        }
+
+    }
+
+
+    public static void main(String... args) {
+        new BaseParser().start();
+    }
 }
