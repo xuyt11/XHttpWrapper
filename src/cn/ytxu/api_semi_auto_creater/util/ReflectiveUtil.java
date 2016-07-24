575809394b9090e70221625beb56aebba380ed0a
xuyt11 <xuytao11@163.com>
2016-07-24 16:52:53 +0800

    添加发射调用的util;

---
+++ src/cn/ytxu/api_semi_auto_creater/util/ReflectiveUtil.java
@@ -0,0 +1,40 @@
+package cn.ytxu.api_semi_auto_creater.util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+/**
+ * Created by ytxu on 2016/7/24.
+ * 发射调用util
+ */
+public class ReflectiveUtil {
+
+    public static Object invokeMethod(Object obj, String methodName) {
+        Class clazz = obj.getClass();
+        try {
+            Method method = clazz.getDeclaredMethod(methodName);
+            return method.invoke(obj);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        throw new IllegalArgumentException("obj:" + clazz.toString() + ", methodName:" + methodName);
+    }
+
+    public static String getString(Object obj, String methodName) {
+        return (String) invokeMethod(obj, methodName);
+    }
+
+    public static List<?> getList(Object obj, String methodName) {
+        return (List<?>) invokeMethod(obj, methodName);
+    }
+
+    public static boolean getBoolean(Object obj, String methodName) {
+        return (boolean) invokeMethod(obj, methodName);
+    }
+
+}
