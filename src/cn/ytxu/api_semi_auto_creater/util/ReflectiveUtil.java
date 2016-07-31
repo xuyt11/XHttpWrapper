df59678fc6d7244329d5ba828a7d42450d600390
xuyt11 <xuytao11@163.com>
2016-07-31 16:47:31 +0800

    fix no such method exception;

--- src/cn/ytxu/api_semi_auto_creater/util/ReflectiveUtil.java
+++ src/cn/ytxu/api_semi_auto_creater/util/ReflectiveUtil.java
@@ -1,5 +1,7 @@
 package cn.ytxu.api_semi_auto_creater.util;
 
+import cn.ytxu.api_semi_auto_creater.model.BaseModel;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -15,14 +17,30 @@ public class ReflectiveUtil {
         try {
             Method method = clazz.getDeclaredMethod(methodName);
             return method.invoke(reflectObj);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
+        } catch (NoSuchMethodException ignore) {
+            Object reflectSuper = invokeMethodFromSuper(reflectObj, methodName, clazz);
+            if (reflectSuper != null) return reflectSuper;
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
         throw new IllegalArgumentException("obj:" + clazz.toString() + ", methodName:" + methodName);
+    }
+
+    private static Object invokeMethodFromSuper(Object reflectObj, String methodName, Class clazz) {
+        if (!(reflectObj instanceof BaseModel)) {
+            throw new IllegalArgumentException(reflectObj.getClass().toString() + " is not extends BaseModel, you need extends it...");
+        }
+
+        // 是数据树中的对象，则调用父对象的方法
+        Object reflectSuper = ((BaseModel) reflectObj).getHigherLevel();
+        if (reflectSuper == null) {// reflectObj is DocModel, so it have not super base model
+            System.out.println("error : the data tree can not call this " + methodName + " method, and return a blank string...");
+            return "";
+        }
+
+        return invokeMethod(reflectSuper, methodName);
     }
 
     public static String getString(Object reflectObj, String methodName) {
