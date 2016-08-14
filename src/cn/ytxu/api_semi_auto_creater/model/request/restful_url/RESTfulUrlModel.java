9bdebc9f68397a5fd1126367c08284282cabdb22
xuyt11 <xuytao11@163.com>
2016-08-14 17:18:47 +0800

    添加多类型参数url的解析；

--- src/cn/ytxu/api_semi_auto_creater/model/request/restful_url/RESTfulUrlModel.java
+++ src/cn/ytxu/api_semi_auto_creater/model/request/restful_url/RESTfulUrlModel.java
@@ -11,6 +11,8 @@ import java.util.List;
 public class RESTfulUrlModel extends BaseModel<RequestModel> {
     private String url;// 方法的相对路径，起始位置必须不是/,因为人
     private boolean isRESTfulUrl = false;
+    private boolean hasMultiParam;// 是否有多选类型的参数，若有的话，则使用url是需要使用multiUrl
+    private String multiUrl;
 
     public RESTfulUrlModel(RequestModel higherLevel, String url) {
         super(higherLevel, null);
@@ -29,6 +31,14 @@ public class RESTfulUrlModel extends BaseModel<RequestModel> {
         this.isRESTfulUrl = isRESTfulUrl;
     }
 
+    public void setHasMultiParam(boolean hasMultiParam) {
+        this.hasMultiParam = hasMultiParam;
+    }
+
+    public void setMultiUrl(String multiUrl) {
+        this.multiUrl = multiUrl;
+    }
+
     public List<?> getFields() {
 
     }
@@ -36,4 +46,5 @@ public class RESTfulUrlModel extends BaseModel<RequestModel> {
     public String RESTful_field_name() {
         return ;
     }
+
 }
--- src/cn/ytxu/api_semi_auto_creater/parser/request/restful_url/RESTfulUrlParser.java
+++ src/cn/ytxu/api_semi_auto_creater/parser/request/restful_url/RESTfulUrlParser.java
@@ -18,12 +18,35 @@ public class RESTfulUrlParser {
 
     public void start() {
         setIsRESTfulUrl();
+        parseMultiUrl();
     }
 
     private void setIsRESTfulUrl() {
         Pattern idOrDatePattern = Pattern.compile("[\\{]{1}.{2,}?[\\}]{1}");
         Matcher m = idOrDatePattern.matcher(model.getUrl());
         model.setRESTfulUrl(m.find());
+    }
+
+    private void parseMultiUrl() {
+        Pattern multiPattern = Pattern.compile("[\\[]{1}.{2,}?[\\]]{1}");
+        Matcher m = multiPattern.matcher(model.getUrl());
+        boolean hasMultiParam = m.find();
+        model.setHasMultiParam(hasMultiParam);
+
+        if (!hasMultiParam) {
+            return;
+        }
+
+        String multiUrl = model.getUrl();
+        do {
+            String group = m.group();
+            if (group.contains("android")) {// 现阶段只匹配不同平台的接口
+                multiUrl = multiUrl.replace(group, "android");// 直接替换requestUrl，不需要进行参数的添加等的处理
+            } else {// 直接抛出异常，在控制台中查看requestUrl、group，再去进行代码的添加
+                throw new RuntimeException("you need fix it, this requestUrl is " + model.getUrl() + ", and the multi pattern group is " + group);
+            }
+        } while (m.find());
+        model.setMultiUrl(multiUrl);
     }
 
 
