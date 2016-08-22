4bbb882677e7fe9273903f82a0d9ff4c489cccff
xuyt11 <xuytao11@163.com>
2016-08-23 00:21:18 +0800

    refactor handle same datatype output ;

--- src/cn/ytxu/api_semi_auto_creater/parser/response/ResponseParser.java
+++ src/cn/ytxu/api_semi_auto_creater/parser/response/ResponseParser.java
@@ -1,12 +1,8 @@
 package cn.ytxu.api_semi_auto_creater.parser.response;
 
-import cn.ytxu.api_semi_auto_creater.model.request.DefinedParamModel;
-import cn.ytxu.api_semi_auto_creater.model.response.OutputParamModel;
 import cn.ytxu.api_semi_auto_creater.model.response.ResponseModel;
-import com.sun.istack.internal.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
+import cn.ytxu.api_semi_auto_creater.parser.response.output.defined.HandleSameDataTypeOutputUtil;
+import cn.ytxu.api_semi_auto_creater.parser.response.output.defined.SetupDefined2OutputUtil;
 
 /**
  * Created by ytxu on 2016/8/16.
@@ -24,8 +20,8 @@ public class ResponseParser {
         new ResponseBodyParser(response).start();
         //2 对request中defineds进行辨析，设置到response字段上
         new SetupDefined2OutputUtil(response).start();
-        // 2.1 若子孙节点中类型相同，如何判断；
-        // 2.2 若兄弟节点中类型相同，如何判断与处理；
+        //2.1 子孙节点或兄弟节点中类型(DataType)相同，判断与处理；
+        new HandleSameDataTypeOutputUtil(response).start();
         //3 获取到相同版本上该Section上的所有的响应体entity，进行遍历判断，
         // 若为一致的（子字段名称与属性都一样的话），则使用相同的entity，不需要重新输出一个新的entity文件
 
--- src/cn/ytxu/api_semi_auto_creater/parser/response/output/defined/SetupDefined2OutputUtil.java
+++ src/cn/ytxu/api_semi_auto_creater/parser/response/output/defined/SetupDefined2OutputUtil.java
@@ -1,4 +1,4 @@
-package cn.ytxu.api_semi_auto_creater.parser.response;
+package cn.ytxu.api_semi_auto_creater.parser.response.output.defined;
 
 import cn.ytxu.api_semi_auto_creater.model.request.DefinedParamModel;
 import cn.ytxu.api_semi_auto_creater.model.response.OutputParamModel;
@@ -7,10 +7,10 @@ import com.sun.istack.internal.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Created by ytxu on 2016/8/21.
+ * 对request中defineds进行辨析，设置到response字段上
  */
 public class SetupDefined2OutputUtil {
 
@@ -24,7 +24,6 @@ public class SetupDefined2OutputUtil {
         List<DefinedParamModel> defineds = getDefineds();
         List<OutputParamModel> outputs = getAllOutput();
         loopSetupDefined2Output(defineds, outputs);
-        setupDontRequireAutomaticGenerationOutputObjectFileOfOutput(outputs);
     }
 
     private List<DefinedParamModel> getDefineds() {
@@ -96,69 +95,5 @@ public class SetupDefined2OutputUtil {
     private boolean findTargetDefined(String outputName, DefinedParamModel defined) {
         return outputName.equals(defined.getName());
     }
-
-
-    //********************** loop not need generation response file to object and array type`s output **********************
-    private void setupDontRequireAutomaticGenerationOutputObjectFileOfOutput(List<OutputParamModel> outputs) {
-        List<OutputParamModel> oaOutputs = getOutputsOfJSONObjectAndJSONArrayType(outputs);
-        List<OutputParamModel> hasDataTypeOutputs = getAllOutputs4HasDataTypeFromOAOutputs(oaOutputs);
-        for (OutputParamModel output : hasDataTypeOutputs) {
-            if (hasSetAutoGenerationTag(output)) {
-                continue;
-            }
-            loopSetDontRequireGenerationResponseEntityFileTag2TheSameDataTypeOutput(output, hasDataTypeOutputs);
-        }
-    }
-
-    private List<OutputParamModel> getOutputsOfJSONObjectAndJSONArrayType(List<OutputParamModel> outputs) {
-        List<OutputParamModel> oaOutputs = new ArrayList<>();
-        for (OutputParamModel output : outputs) {
-            switch (output.getType()) {
-                case JSON_OBJECT:
-                case JSON_ARRAY:
-                    oaOutputs.add(output);
-                    break;
-            }
-        }
-        return oaOutputs;
-    }
-
-    private List<OutputParamModel> getAllOutputs4HasDataTypeFromOAOutputs(List<OutputParamModel> oaOutputs) {
-        List<OutputParamModel> hasDataTypes = new ArrayList<>();
-        for (OutputParamModel output : oaOutputs) {
-            if (notHasDataType(output)) {
-                continue;
-            }
-            hasDataTypes.add(output);
-        }
-        return hasDataTypes;
-    }
-
-    private boolean notHasDataType(OutputParamModel output) {
-        return Objects.isNull(output.getDefined()) || Objects.isNull(output.getDefined().getDataType());
-    }
-
-    private boolean hasSetAutoGenerationTag(OutputParamModel output) {
-        return output.isDontRequireGenerationResponseEntityFileTag();
-    }
-
-    /**
-     * 将oaOutputs中多有defined相同的output，设置为不需要再生成response entity file的
-     */
-    private void loopSetDontRequireGenerationResponseEntityFileTag2TheSameDataTypeOutput(OutputParamModel target, List<OutputParamModel> hasDataTypeOutputs) {
-        for (OutputParamModel output : hasDataTypeOutputs) {
-            if (output == target) {
-                continue;
-            }
-            if (isTheSameDataType(target, output)) {
-                output.setDontRequireGenerationResponseEntityFileTag();
-            }
-        }
-    }
-
-    private boolean isTheSameDataType(OutputParamModel target, OutputParamModel output) {
-        return target.getDefined().getDataType().equals(output.getDefined().getDataType());
-    }
-
 
 }
