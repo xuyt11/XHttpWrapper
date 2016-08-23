45ea67a0ab34779f4a34d1712583b8e6448d7dc9
xuyt11 <xuytao11@163.com>
2016-08-24 07:30:17 +0800

    若在defined中已经定义好了DataType，则不需要处理3逻辑了； ----因为所有的逻辑，都是后台接口开发人员设置好了的。 如：相同类型的对象，后台开发都会设置相同的DataType，那就不需要我们前台开发去逐一查看到底哪个是一致的对象实体；

--- src/cn/ytxu/api_semi_auto_creater/parser/response/ResponseParser.java
+++ src/cn/ytxu/api_semi_auto_creater/parser/response/ResponseParser.java
@@ -20,11 +20,7 @@ public class ResponseParser {
         new ResponseBodyParser(response).start();
         //2 对request中defineds进行辨析，设置到response字段上
         new SetupDefined2OutputUtil(response).start();
-        //2.1 子孙节点或兄弟节点中类型(DataType)相同，判断与处理；
+        //3 子孙节点或兄弟节点中类型(DataType)相同，判断与处理；
         new HandleSameDataTypeOutputUtil(response).start();
-        //3 获取到相同版本上该Section上的所有的响应体entity，进行遍历判断，
-        // 若为一致的（子字段名称与属性都一样的话），则使用相同的entity，不需要重新输出一个新的entity文件
-
-
     }
 }
