d1b9deb511721851adb4e797414707bb91a61983
xuyt11 <xuytao11@163.com>
2016-09-01 09:03:52 +0800

    添加一个过滤功能的配置，提高一点点速度；

--- src/cn/ytxu/api_semi_auto_creater/config/property/FilterProperty.java
+++ src/cn/ytxu/api_semi_auto_creater/config/property/FilterProperty.java
@@ -7,7 +7,6 @@ import cn.ytxu.api_semi_auto_creater.model.base.VersionModel;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
  * Created by ytxu on 2016/8/28.
  */
@@ -16,6 +15,7 @@ public class FilterProperty {// 需要输出的版本号列表
     private static FilterProperty instance;
 
     private PropertyEntity.FilterBean filter;
+    private boolean useHeaderFilter = false;// 是否使用过滤request header的功能
 
     private FilterProperty(PropertyEntity.FilterBean filter) {
         this.filter = filter;
@@ -27,12 +27,20 @@ public class FilterProperty {// 需要输出的版本号列表
 
     public static void load(PropertyEntity.FilterBean filter) {
         instance = new FilterProperty(filter);
+        configHeaderFilterFun(filter);
+    }
+
+    private static void configHeaderFilterFun(PropertyEntity.FilterBean filter) {
+        instance.useHeaderFilter = filter.getHeaders().size() > 0;
     }
 
     /**
      * 过滤request中header参数
      */
     public boolean hasThisHeaderInFilterHeaders(String headerName) {
+        if (!useHeaderFilter) {
+            return false;
+        }
         for (String filterHeader : filter.getHeaders()) {
             if (filterHeader.equals(headerName)) {
                 return true;
