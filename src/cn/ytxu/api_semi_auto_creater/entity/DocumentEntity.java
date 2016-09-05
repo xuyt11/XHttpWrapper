50869d5f943b3b5dd67b4528370b765868181784
xuyt11 <xuytao11@163.com>
2016-09-05 23:25:24 +0800

    delete status codes;

--- src/cn/ytxu/api_semi_auto_creater/entity/DocumentEntity.java
+++ src/cn/ytxu/api_semi_auto_creater/entity/DocumentEntity.java
@@ -10,9 +10,7 @@ import java.util.List;
  * 只有一个状态码类: 状态码不分版本;也就是说,状态码是不能更改的,只能是新增;
  */
 public class DocumentEntity extends BaseEntity {
-    // TODO 设置version entity enum，所以以后每新加一个版本，都要进行添加，查找不到该版本号，直接抛出异常
     private List<String> versions;// API所有的版本号
-    private List<StatusCodeEntity> statusCodes;// 所有的状态码
     private List<SectionEntity> sections;
 
     public DocumentEntity(BaseEntity higherLevel, Element element) {
@@ -26,14 +24,6 @@ public class DocumentEntity extends BaseEntity {
 
     public void setVersions(List<String> versions) {
         this.versions = versions;
-    }
-
-    public List<StatusCodeEntity> getStatusCodes() {
-        return statusCodes;
-    }
-
-    public void setStatusCodes(List<StatusCodeEntity> statusCodes) {
-        this.statusCodes = statusCodes;
     }
 
     public List<SectionEntity> getSections() {
