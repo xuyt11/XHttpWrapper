b88dd0428c068e783ff760c03705feb227665e0b
xuyt11 <xuytao11@163.com>
2016-09-11 22:11:38 +0800

    添加example comment；

--- src/cn/ytxu/restful/apidocjs/bean/Bean.java
+++ src/cn/ytxu/restful/apidocjs/bean/Bean.java
@@ -3,12 +3,23 @@ package cn.ytxu.restful.apidocjs.bean;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * request:header,parameter
+ * response:success,error
+ *
+ */
 public class Bean {
     /**
+     * 在request与response中的field
      * k:field group name
+     * 1，若为response，则为parent name
      * v:fields
      */
     private Map<String, List<FieldBean>> fields;
+    /**
+     * 若为request，则为field value example
+     * 若为response，则为该response example
+     */
     private List<ExampleBean> examples;
 
     public Map<String, List<FieldBean>> getFields() {
