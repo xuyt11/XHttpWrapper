0a86c6da79ebc8ca7d38ff9644d0eed78596f97d
xuyt11 <xuytao11@163.com>
2016-09-23 23:27:32 +0800

    add field example model;

--- src/cn/ytxu/xhttp_wrapper/model/response/ResponseGroupModel.java
+++ src/cn/ytxu/xhttp_wrapper/model/response/ResponseGroupModel.java
@@ -2,6 +2,7 @@ package cn.ytxu.xhttp_wrapper.model.response;
 
 import cn.ytxu.xhttp_wrapper.apidocjs.bean.Bean;
 import cn.ytxu.xhttp_wrapper.model.BaseModel;
+import cn.ytxu.xhttp_wrapper.model.field.FieldExampleModel;
 import cn.ytxu.xhttp_wrapper.model.field.FieldGroupContainer;
 import cn.ytxu.xhttp_wrapper.model.field.FieldGroupModel;
 import cn.ytxu.xhttp_wrapper.model.request.RequestModel;
@@ -12,6 +13,7 @@ import java.util.List;
 public class ResponseGroupModel extends BaseModel<RequestModel, Bean> implements FieldGroupContainer<ResponseGroupModel> {
 
     private List<FieldGroupModel<ResponseGroupModel>> fieldGroups = Collections.EMPTY_LIST;
+    private List<FieldExampleModel<ResponseGroupModel>> fieldExamples = Collections.EMPTY_LIST;
 
     public ResponseGroupModel(RequestModel higherLevel, Bean element) {
         super(higherLevel, element);
@@ -27,5 +29,11 @@ public class ResponseGroupModel extends BaseModel<RequestModel, Bean> implements
         this.fieldGroups = fieldGroups;
     }
 
+    public List<FieldExampleModel<ResponseGroupModel>> getFieldExamples() {
+        return fieldExamples;
+    }
 
+    public void setFieldExamples(List<FieldExampleModel<ResponseGroupModel>> fieldExamples) {
+        this.fieldExamples = fieldExamples;
+    }
 }
