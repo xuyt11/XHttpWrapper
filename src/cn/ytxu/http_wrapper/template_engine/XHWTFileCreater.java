a9d9e1fdf2c8c45411f627c0331d4a72a850fcdd
xuyt11 <xuytao11@163.com>
2016-12-16 08:24:04 +0800

    remove base templete file creater;

--- src/cn/ytxu/http_wrapper/template_engine/XHWTFileCreater.java
+++ src/cn/ytxu/http_wrapper/template_engine/XHWTFileCreater.java
@@ -1,13 +1,23 @@
 package cn.ytxu.http_wrapper.template_engine;
 
+import cn.ytxu.http_wrapper.common.util.FileUtil;
 import cn.ytxu.http_wrapper.common.util.LogUtil;
+import cn.ytxu.http_wrapper.config.ConfigWrapper;
 import cn.ytxu.http_wrapper.model.version.VersionModel;
-import cn.ytxu.http_wrapper.template_engine.creater.XHWTFileBaseCreater;
 import cn.ytxu.http_wrapper.template_engine.creater.XHWTFileType;
 import cn.ytxu.http_wrapper.template_engine.parser.model.XHWTModel;
 import cn.ytxu.http_wrapper.template_engine.parser.XHWTFileParser;
+import cn.ytxu.http_wrapper.template_engine.parser.statement.StatementRecord;
+import cn.ytxu.http_wrapper.template_engine.parser.statement.record.TextStatementRecord;
+import cn.ytxu.http_wrapper.template_engine.parser.statement.record.retain.RetainModel;
+import cn.ytxu.http_wrapper.template_engine.parser.statement.record.retain.RetainParser;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by Administrator on 2016/9/7.
@@ -51,7 +61,51 @@ public class XHWTFileCreater {
     }
 
     private void generateTargetFileByReflectData(XHWTModel tModel, Object reflectData) {
-        XHWTFileBaseCreater.writeContent2TargetFileByXTempAndReflectModel(tModel, reflectData);
+        String dirPath = getString(tModel.getFileDir(), reflectData);
+        String fileName = getString(tModel.getFileName(), reflectData);
+
+        getRetainAndWriter4TargetFile(dirPath, fileName, (writer, retain) -> {
+            StringBuffer contentBuffer = StatementRecord.getWriteBuffer(tModel.getRecords(), reflectData, retain);
+            writer.write(contentBuffer.toString());
+        });
+    }
+
+    private static String getString(String content, Object reflectModel) {
+        TextStatementRecord record = new TextStatementRecord(null, content);
+        record.parse();
+        return record.getWriteBuffer(reflectModel, null).toString().trim();
+    }
+
+    private static void getRetainAndWriter4TargetFile(String dirPath, String fileName, OnGetWriter onGetWriter) {
+        if (Objects.isNull(onGetWriter)) {
+            throw new RuntimeException("OnGetWriter listener is null...");
+        }
+
+        Writer writer = null;
+        try {
+            RetainModel retain = RetainParser.getRetainByFile(dirPath, fileName);
+            String fileCharset = ConfigWrapper.getBaseConfig().getCreateFileCharset();
+            writer = FileUtil.getWriter(dirPath, fileName, fileCharset);
+            onGetWriter.onGetWriter(writer, retain);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (Objects.nonNull(writer)) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private interface OnGetWriter {
+        void onGetWriter(Writer writer, RetainModel retain) throws IOException;
     }
 
 }
